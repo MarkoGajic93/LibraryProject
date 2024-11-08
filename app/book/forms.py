416eb1de1abd3090ccdb7dc8b0e42823468ed2b0
Marko Gajic <mgajic@bluecatnetworks.com>
2024-11-08 20:48:54 +0100

    Created flask form for adding new book and implemented new book route

---
+++ app/book/forms.py
@@ -0,0 +1,35 @@
+from typing import Literal
+
+from flask_wtf import FlaskForm
+from wtforms.fields.choices import SelectField
+from wtforms.fields.numeric import IntegerField
+from wtforms.fields.simple import StringField, SubmitField
+from wtforms.validators import InputRequired, DataRequired
+
+
+class NewBookForm(FlaskForm):
+    title = StringField("Title",
+                        validators=[InputRequired("Input is required!"),
+                                    DataRequired("Data is required!")])
+
+    year_published = SelectField("Year published",
+                                 choices=[(str(year), str(year)) for year in range(1900, 2024)],
+                                 validators=[InputRequired("Input is required!")])
+
+    author = SelectField("Author")
+
+    warehouse = SelectField("Warehouse")
+
+    quantity = IntegerField("Quantity",
+                            validators=[InputRequired("Input is required!"),
+                                        DataRequired("Data is required!")])
+
+    submit = SubmitField("Add book")
+
+    def set_choices(self, cursor, field: Literal["author", "warehouse"]):
+        cursor.execute(f"SELECT id, name FROM {field}")
+        options = cursor.fetchall()
+        if field == "author":
+            self.author.choices = options
+        if field == "warehouse":
+            self.warehouse.choices = options
--- app/book/routes.py
+++ app/book/routes.py
@@ -1,6 +1,32 @@
+from flask import render_template, flash, url_for
+from werkzeug.utils import redirect
+
 from app.book import book_bp
+from app.book.forms import NewBookForm
+from db.db_service import get_db
 
 
-@book_bp("/new", methods=["GET", "POST"])
-def add_new_book():
-    pass
+@book_bp.route("/new", methods=["GET", "POST"])
+def add_new():
+    conn = get_db()
+    cursor = conn.cursor()
+    form = _setup_form(cursor)
+    if form.validate_on_submit():
+        cursor.execute("""INSERT INTO book (title, year_published, author_id) VALUES (%s,%s,%s) RETURNING id;""",
+                       (form.title.data, form.year_published.data, form.author.data))
+        print("book inserted into a table")
+        book_uuid = cursor.fetchone()[0]
+        cursor.execute("""INSERT INTO warehouse_book (warehouse_id, book_id, quantity) VALUES (%s,%s,%s)""",
+                       (form.warehouse.data, book_uuid, form.quantity.data))
+        conn.commit()
+        flash(f"Book: {form.title.data} added successfully.", "success")
+        return redirect(url_for("home.home"))
+
+    return render_template("new_book.html", form=form)
+
+
+def _setup_form(cursor) -> NewBookForm:
+    form = NewBookForm()
+    form.set_choices(cursor, "author")
+    form.set_choices(cursor, "warehouse")
+    return form
