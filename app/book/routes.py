1609e4fdc517ab1f83dbd076e3c48fdc5e725a3a
Marko Gajic <mgajic@bluecatnetworks.com>
2024-11-09 14:45:33 +0100

    Fixed add new book route and created a route for managing book copies in warehouses

--- app/book/routes.py
+++ app/book/routes.py
@@ -15,10 +15,13 @@ def add_new():
     form = _setup_form(cursor)
 
     if form.validate_on_submit():
+        # Check if book already exist
+        # (NOTE: criteria - books with same title, author and publish year are the same books, with same id)
         cursor.execute("""SELECT id FROM book WHERE title=%s AND year_published=%s AND author_id=%s""",
                        (form.title.data.upper(), form.year_published.data, form.author.data))
         book_id = cursor.fetchone()
 
+        # If not found - insert new record
         if not book_id:
             cursor.execute("""INSERT INTO book (title, year_published, author_id) VALUES (%s,%s,%s) RETURNING id;""",
                            (form.title.data.upper(), form.year_published.data, form.author.data))
@@ -26,18 +29,19 @@ def add_new():
             cursor.execute("""INSERT INTO warehouse_book (warehouse_id, book_id, quantity) VALUES (%s,%s,%s)""",
                            (form.warehouse.data, book_id, form.quantity.data))
 
+        # If found - no interaction with the book table, just update quantity in the warehouse
         else:
-            cursor.execute("""SELECT warehouse_id, quantity FROM warehouse_book WHERE book_id=%s""", (book_id,))
-            warehouse_id, quantity = cursor.fetchone()
-
-            if warehouse_id != form.warehouse.data:
+            # Check if book already have some copies in the selected warehouse
+            cursor.execute("""SELECT warehouse_id, quantity FROM warehouse_book WHERE book_id=%s AND warehouse_id=%s""",
+                           (book_id, form.warehouse.data))
+            try:
+                warehouse_id, quantity = cursor.fetchone()
+                cursor.execute("""UPDATE warehouse_book SET quantity=%s WHERE warehouse_id=%s AND book_id=%s""",
+                               (quantity + form.quantity.data, warehouse_id, book_id))
+            # If it doesn't, insert new entry in warehouse_book
+            except TypeError:
                 cursor.execute("""INSERT INTO warehouse_book (warehouse_id, book_id, quantity) VALUES (%s,%s,%s)""",
                                (form.warehouse.data, book_id, form.quantity.data))
-
-            else:
-                cursor.execute("""UPDATE warehouse_book SET quantity=%s WHERE warehouse_id=%s AND book_id=%s""",
-                               (quantity+form.quantity.data, warehouse_id, book_id))
-
         conn.commit()
         flash(f"Book: {form.title.data.upper()} added successfully.", "success")
         return redirect(url_for("home.home"))
@@ -61,6 +65,35 @@ def book(book_id: uuid.UUID):
     if book_data:
         book_dict = next(iter(generate_book_dict(book_data).values()))
         return render_template("book.html", book=book_dict, deleteAllBooksForm=delete_all_books_form)
+    else:
+        flash("That book doesnt exist", "danger")
+        return redirect(url_for("home.home"))
+
+@book_bp.route("/edit/<uuid:book_id>", methods=["GET", "POST"])
+def manage_copies(book_id: uuid.UUID):
+    conn = get_db()
+    cursor = conn.cursor()
+
+    book_data = get_book_data(cursor, book_id)
+    book_dict = {}
+    if book_data:
+        edit_form = EditBookWarehouseCopies()
+        book_dict = next(iter(generate_book_dict(book_data).values()))
+        cursor.execute("""SELECT id, name FROM warehouse""")
+        warehouses = cursor.fetchall()
+        edit_form.warehouse.choices = warehouses
+        if edit_form.validate_on_submit():
+            selected_warehouse = next((warehouse[1] for warehouse in warehouses if warehouse[0] == edit_form.warehouse.data))
+            if selected_warehouse in book_dict["warehouses"].keys():
+                cursor.execute("""UPDATE warehouse_book SET quantity=%s WHERE warehouse_id=%s AND book_id=%s""",
+                               (edit_form.quantity.data, edit_form.warehouse.data, book_dict["id"]))
+            else:
+                cursor.execute("""INSERT INTO warehouse_book (warehouse_id, book_id, quantity) VALUES (%s,%s,%s)""",
+                               (edit_form.warehouse.data, book_dict["id"], edit_form.quantity.data))
+            conn.commit()
+            flash(f"Book {book_dict['title']} updated successfully.", "success")
+            return redirect(url_for('book.book', book_id=book_id))
+        return render_template("edit_copies.html", book=book_dict, editForm=edit_form)
     else:
         flash("That book doesnt exist", "danger")
         return redirect(url_for("home.home"))
