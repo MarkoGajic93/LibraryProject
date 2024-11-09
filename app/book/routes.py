d3d7f825e4bbf227133273c602d6eb88179375e6
Marko Gajic <mgajic@bluecatnetworks.com>
2024-11-09 01:01:39 +0100

    Fixed adding new books - added check if the book already exist

--- app/book/routes.py
+++ app/book/routes.py
@@ -11,15 +11,33 @@ def add_new():
     conn = get_db()
     cursor = conn.cursor()
     form = _setup_form(cursor)
+
     if form.validate_on_submit():
-        cursor.execute("""INSERT INTO book (title, year_published, author_id) VALUES (%s,%s,%s) RETURNING id;""",
-                       (form.title.data, form.year_published.data, form.author.data))
-        print("book inserted into a table")
-        book_uuid = cursor.fetchone()[0]
-        cursor.execute("""INSERT INTO warehouse_book (warehouse_id, book_id, quantity) VALUES (%s,%s,%s)""",
-                       (form.warehouse.data, book_uuid, form.quantity.data))
+        cursor.execute("""SELECT id FROM book WHERE title=%s AND year_published=%s AND author_id=%s""",
+                       (form.title.data.upper(), form.year_published.data, form.author.data))
+        book_id = cursor.fetchone()
+
+        if not book_id:
+            cursor.execute("""INSERT INTO book (title, year_published, author_id) VALUES (%s,%s,%s) RETURNING id;""",
+                           (form.title.data.upper(), form.year_published.data, form.author.data))
+            book_id = cursor.fetchone()
+            cursor.execute("""INSERT INTO warehouse_book (warehouse_id, book_id, quantity) VALUES (%s,%s,%s)""",
+                           (form.warehouse.data, book_id, form.quantity.data))
+
+        else:
+            cursor.execute("""SELECT warehouse_id, quantity FROM warehouse_book WHERE book_id=%s""", (book_id,))
+            warehouse_id, quantity = cursor.fetchone()
+
+            if warehouse_id != form.warehouse.data:
+                cursor.execute("""INSERT INTO warehouse_book (warehouse_id, book_id, quantity) VALUES (%s,%s,%s)""",
+                               (form.warehouse.data, book_id, form.quantity.data))
+
+            else:
+                cursor.execute("""UPDATE warehouse_book SET quantity=%s WHERE warehouse_id=%s AND book_id=%s""",
+                               (quantity+form.quantity.data, warehouse_id, book_id))
+
         conn.commit()
-        flash(f"Book: {form.title.data} added successfully.", "success")
+        flash(f"Book: {form.title.data.upper()} added successfully.", "success")
         return redirect(url_for("home.home"))
 
     return render_template("new_book.html", form=form)
