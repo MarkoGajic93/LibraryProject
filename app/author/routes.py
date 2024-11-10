e6e8e9c9c5160b4bda4cbe4110fe625712e93a21
Marko Gajic <mgajic@bluecatnetworks.com>
2024-11-10 14:19:18 +0100

    Created add new author route and updated authors HTML

--- app/author/routes.py
+++ app/author/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, url_for
+from werkzeug.utils import redirect
 
 from app.author import author_bp
+from app.author.forms import NewAuthorForm
 from db.db_service import get_db
 
 
@@ -19,3 +21,16 @@ def view_all():
         }
         authors.append(author)
     return render_template("authors.html", authors=authors)
+
+@author_bp.route("/new", methods=["GET", "POST"])
+def add_new():
+    conn = get_db()
+    cursor = conn.cursor()
+    form = NewAuthorForm()
+    if form.validate_on_submit():
+        cursor.execute("""INSERT INTO author (name, biography) VALUES (%s, %s)""",
+                       (form.name.data, form.biography.data))
+        conn.commit()
+        flash(f"Author {form.name.data} added successfully.", "success")
+        return redirect(url_for("author.view_all"))
+    return render_template("new_author.html", form=form)
