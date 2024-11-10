d84ff19d66b4d0d3a1d4c6eaaccf0b5a6886d2a4
Marko Gajic <mgajic@bluecatnetworks.com>
2024-11-10 14:44:33 +0100

    Created delete author route and updated authors HTML

--- app/author/routes.py
+++ app/author/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, url_for
 from werkzeug.utils import redirect
 
 from app.author import author_bp
-from app.author.forms import NewAuthorForm
+from app.author.forms import NewAuthorForm, DeleteAuthorForm
 from db.db_service import get_db
 
 
@@ -34,3 +34,16 @@ def add_new():
         flash(f"Author {form.name.data} added successfully.", "success")
         return redirect(url_for("author.view_all"))
     return render_template("new_author.html", form=form)
+
+@author_bp.route("/delete", methods=["GET", "POST"])
+def delete():
+    conn = get_db()
+    cursor = conn.cursor()
+    form = DeleteAuthorForm()
+    form.set_choices(cursor)
+    if form.validate_on_submit():
+        cursor.execute("""DELETE FROM author WHERE id=%s""", (form.name.data,))
+        conn.commit()
+        flash("Author deleted successfully.", "success")
+        return redirect(url_for("author.view_all"))
+    return render_template("delete_author.html", form=form)
