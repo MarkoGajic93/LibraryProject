01eb0f40a29026cf97e11d6153419ea7cd2370a0
Marko Gajic <mgajic@bluecatnetworks.com>
2024-11-12 00:34:55 +0100

    Implemented get current user function and injected current user into all templates

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, url_for, session
+from flask import render_template, flash, url_for, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
@@ -6,6 +6,10 @@ from app.auth import auth_bp
 from app.auth.forms import MemberRegisterForm, MemberLoginForm
 from db.db_service import get_db
 
+
+@auth_bp.app_context_processor
+def inject_current_user():
+    return dict(current_user=get_current_user())
 
 @auth_bp.route("/register", methods=["GET", "POST"])
 def register():
@@ -27,10 +31,10 @@ def login():
     cursor = conn.cursor()
     form = MemberLoginForm()
     if form.validate_on_submit():
-        cursor.execute("""SELECT name, password FROM member WHERE email=%s""", (form.email.data,))
-        name, hash_password = cursor.fetchone()
+        cursor.execute("""SELECT email, name, password FROM member WHERE email=%s""", (form.email.data,))
+        email, name, hash_password = cursor.fetchone()
         if check_password_hash(hash_password, form.password.data):
-            session["user"] = name
+            session["user"] = {'email': email, 'name': name}
             flash(f"{name} logged in successfully", "success")
             return redirect(url_for("home.home"))
         flash("Incorrect password", "danger")
@@ -40,7 +44,20 @@ def login():
 def logout():
     try:
         user = session.pop("user")
-        flash(f"{user} logged out.", "success")
+        flash(f"{user['name']} logged out.", "success")
     except KeyError:
         flash("You are not logged in.", "danger")
     return redirect(url_for("home.home"))
+
+def get_current_user() -> dict:
+    _current_user = getattr(g, "_current_user", None)
+    if not _current_user:
+        if session.get("user"):
+            conn = get_db()
+            cursor = conn.cursor()
+            cursor.execute("""SELECT email, name, age, phone_number FROM member WHERE email=%s""", (session['user']['email'],))
+            email, name, age, phone = cursor.fetchone()
+            _current_user = g._current_user = {"email": email, "name": name, "age": age, "phone": phone}
+        else:
+            _current_user = {}
+    return _current_user
